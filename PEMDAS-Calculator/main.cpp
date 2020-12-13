647b00aa993210e4e872cdd1e321f08b310bad5c
Mahmoud AlSofyani <msafar95@hotmail.com>
2020-12-13 23:53:44 +0400

    making progress I need to debug a few things

--- PEMDAS-Calculator/main.cpp
+++ PEMDAS-Calculator/main.cpp
@@ -11,11 +11,27 @@ void showQueue(std::queue<int>);
 void clearQueue(std::queue<int>& outputQueue);
 void clearStack(std::stack<char>& operatorStack);
 bool checkIfNumber(char character);
+int getPrecedence(char character);
+int getAssociativity(char character);
 
 int main() {
 
-	//std::vector<char> operators{ '+', '-', '/', '*', '(', ')' };
-	std::map<char, int> operators;
+	struct Operators {
+		char op;
+		int precendence;
+		//0 -> Right
+		//1 -> Left
+		int associativity;
+	};
+
+	Operators operations[5];
+
+	operations[0] = { '+', 0, 1 };
+	operations[1] = { '-', 0, 1 };
+	operations[2] = { '/', 1, 1 };
+	operations[3] = { '*', 1, 1 };
+	operations[4] = { '^', 2, 0 };
+
 	std::string expression;
 	std::queue<int> outputQueue;
 	std::stack<char> operatorStack;
@@ -23,38 +39,50 @@ int main() {
 
 	std::cout << "Enter expression" << std::endl;
 	std::getline(std::cin, expression);
+	expression.erase(remove(expression.begin(), expression.end(), ' '), expression.end());
 
-	operators.insert(std::pair<char, int>('(', 1));
-	operators.insert(std::pair<char, int>(')', 1));
-	operators.insert(std::pair<char, int>('^', 2));
-	operators.insert(std::pair<char, int>('*', 3));
-	operators.insert(std::pair<char, int>('/', 3));
-	operators.insert(std::pair<char, int>('+', 4));
-	operators.insert(std::pair<char, int>('-', 4));
+	int opIndex;
 
-	for (i = 0; i < expression.length(); i++) {
+	for (int i = 0; i < expression.length(); i++) {
+
 		int index = i;
-		char expressionToCheck = expression[i];
-		if (isdigit(expression[i])) {
+		if (isdigit(expression[index])) {
 			std::string fullNumber;
-
-			while (checkIfNumber(expression[index])) {
+			while (isdigit(expression[index])) {
 				fullNumber += expression[index];
 				index++;
 			}
 			outputQueue.push(std::stoi(fullNumber));
 		}
-		else {
-			std::map<char, int>::iterator mapIterator;
-			for (mapIterator = operators.begin(); mapIterator != operators.end(); ++mapIterator) {
-				if (mapIterator->first == expression[i]) {
-					operatorStack.push(expression[i]);
+
+		if (expression[index] == '+' || expression[index] == '-' || expression[index] == '/' || expression[index] == '*' || expression[index] == '^') {
+			for (int j = 0; j < 5; j++) {
+				if (expression[index] == operations[j].op) {
+					opIndex = j;
 				}
 			}
+
+			while (!operatorStack.empty() && (operatorStack.top() == '+' || operatorStack.top() == '-' || operatorStack.top() == '/' || operatorStack.top() == '*' || operatorStack.top() == '^')) {
+				if ((getAssociativity(expression[index]) == 1 && (getPrecedence(expression[index]) <= getPrecedence(operatorStack.top()))) || (getAssociativity(expression[index]) == 0 && (getPrecedence(expression[index]) < getPrecedence(operatorStack.top())))) {
+					outputQueue.push(operatorStack.top());
+					operatorStack.pop();
+				}
+			}
+			operatorStack.push(expression[index]);
+		}
+
+		if (expression[index] == '(') {
+			operatorStack.push(expression[index]);
+		}
+		if (expression[index] == ')') {
+			while (!operatorStack.empty() && operatorStack.top() != '(') {
+				outputQueue.push(operatorStack.top());
+				operatorStack.pop();
+			}
+			operatorStack.pop();
 		}
 		i = index;
 	}
-
 
 	std::cout << "Operator Stack: " << std::endl;
 	showStack(operatorStack);
@@ -72,7 +100,7 @@ int main() {
 
 void showStack(std::stack<char> operatorStack) {
 	while (!operatorStack.empty()) {
-		std::cout << operatorStack.top() << " ";
+		std::cout << static_cast<char>(operatorStack.top()) << " ";
 		operatorStack.pop();
 	}
 	std::cout << std::endl;
@@ -86,16 +114,46 @@ void showQueue(std::queue<int> outputQueue) {
 	std::cout << std::endl;
 }
 
-void clearQueue(std::queue<int> &outputQueue) {
+void clearQueue(std::queue<int>& outputQueue) {
 	std::queue<int> emptyQueue;
 	std::swap(outputQueue, emptyQueue);
 }
 
-void clearStack(std::stack<char> &operatorStack) {
+void clearStack(std::stack<char>& operatorStack) {
 	std::stack<char> emptyStack;
 	std::swap(operatorStack, emptyStack);
 }
 
 bool checkIfNumber(char character) {
-	return (isdigit(character) ? true :  false);
+	return (isdigit(character) ? true : false);
+}
+
+int getPrecedence(char character) {
+	switch (character) {
+	case '+':
+	case '-': {
+		return 0;
+	}
+	case '/':
+	case '*': {
+		return 1;
+	}
+	case '^': {
+		return 2;
+	}
+	}
+}
+
+int getAssociativity(char character) {
+	switch (character) {
+	case '+':
+	case '-':
+	case '/':
+	case '*': {
+		return 1;
+	}
+	case '^': {
+		return 0;
+	}
+	}
 }
